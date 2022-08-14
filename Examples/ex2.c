af5295070e82efe0ec138f39a1ddad7389f8ab6b
macintAsh1984 <ashh.valdez@gmail.com>
2022-08-13 21:24:22 -0700

    Updated Comment In ex2.c

--- Examples/ex2.c
+++ Examples/ex2.c
@@ -1,8 +1,10 @@
 /**
- * Suppose we now have multiple streets, each street has some houses,
- * and each house has multiple stories. 
+ * Pointers: Example 2
+ *
+ * Suppose we now have multiple streets. Each street has some houses, and each house has multiple stories. 
+ * 
  * How can we put the number 15 on a given floor of a given house on a given street?
- * Declare a 3D array and put number 10 in one of the locations
+ * Declare a 3D array and put number 10 in one of the locations.
  * */
 
 #include <stdio.h>
